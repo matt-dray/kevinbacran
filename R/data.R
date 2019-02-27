b2441281c75b3aebba780efbb70ec243fc63fe4d
matt-dray <mwdray@gmail.com>
2019-02-27 22:32:02 +0000

    Upload newer, clean author name data, fix data doc

--- R/data.R
+++ R/data.R
@@ -1,9 +1,10 @@
 #' Package-Author tidygraph from a CRAN database snapshot
 #'
-#' A tidygraph object of all package-author relationships as captured on 26
+#' A tidygraph object of all package-author relationships as captured on 27
 #' February 2019. Extracted using the \code{kevinbacran::kb_combos()} function.
+#' Intended as an example for testing the other functions in the package.
 #'
-#' @format A large tbl_graph (16911 nodes, 99542 edges, 10 elements, 5.8 Mb)
+#' @format A large tbl_graph (16915 nodes, 99560 edges, 10 elements, 5.8 Mb)
 #' \describe{
 #'   \item{Nodes}{package authors}
 #'   \item{Edges}{package names}
@@ -11,7 +12,7 @@
 #'
 #' @docType data
 #'
-#' @usage data(cran_pkg_data)
+#' @usage data(cran_pkg_graph)
 #'
 #' @source \url{https://cran.r-project.org/}
 "cran_pkg_graph"
